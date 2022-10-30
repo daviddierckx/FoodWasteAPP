46dc56d5c62b0b665b7e04a25312cab221e37b5f
daviddierckx <62950437+daviddierckx@users.noreply.github.com>
2022-10-30 21:09:28 +0100

    last

--- FoodWast.API/Program.cs
+++ FoodWast.API/Program.cs
@@ -81,8 +81,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
     app.UseSwagger();
-    app.UseSwaggerUI();
-
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FoodWaste API V1");
+        c.RoutePrefix = string.Empty;
+    });
 
 app.UseHttpsRedirection();
 
